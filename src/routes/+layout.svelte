4f4eba9825ce500661132e2655e1c0ff8d82ba21
JershDervis <joshdavis16@outlook.com>
2025-07-06 17:10:58 +1000

    Only use speed insights in prod build

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
 	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+	import { dev } from '$app/environment';
 	import { Toaster } from '$lib/components/ui/sonner';
 	import '../app.css';
 	import { ModeWatcher } from 'mode-watcher';
 	import ThemeSwitcher from '$lib/components/theme-switcher.svelte';
 
-	injectSpeedInsights();
+	if (!dev) {
+		injectSpeedInsights();
+	}
 
 	let { children } = $props();
 </script>
